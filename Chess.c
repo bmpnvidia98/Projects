27d0e332e2a91c94ffcbf43ad22b3f2f1154d457
Brian P <antoun@localhost.localdomain>
2015-11-17 02:42:45 -0500

    Cs Added pawn movementn

--- Chess.c
+++ Chess.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <ctype.h>
+#include <math.h>
 
 char board[8][8] = {{'r','n','b','k','q','b','n','r'},{'p','p','p','p','p','p','p','p'},{'_','_','_','_','_','_','_','_'},{'_','_','_','_','_','_','_','_'},{'_','_','_','_','_','_','_','_'},{'_','_','_','_','_','_','_','_'},{'P','P','P','P','P','P','P','P'},{'R','N','B','K','Q','B','N','R'}};
 
@@ -17,16 +18,51 @@ void printb(){
 	putchar('\n');
 	putchar('\n');
 }
+
+int isvalid(char piece, char ix1, char iy1, char mov, char fx1, char fy1);
+	
+
 int main(){
 	char ix, iy, mov, fx, fy; // Initial X, Initial Y, Move, Final X, Final Y
 	printb();
+	moving:
+	printf("Make your move:\n");
 	scanf("%c%c%c%c%c",ix, iy, mov, fx, fy);
 		if(ix == ' ' ||iy == ' ' ||mov == ' ' ||fx == ' ' ||fy == ' '){
 			printf("You must enter in format (X-coord)(Y-coord)(Move/attack)(X-coord)(Y-coord)\n");
 			printf("Moves are denoted as \"-\", and attacks are \"x\"\n");
+			goto moving;
 				}
 		
+	
 
 	printb();
 	return 0;
+}
+
+int isvalid(char piece, char ix1, char iy1, char mov, char fx1, char fy1){
+	int result;
+	int fx = fx1 - 'a';
+	int ix = ix1 - 'a'; // Converting chars to int for easier use later
+	int iy = iy1 - '0';	
+	int fy = fy1 - '0';
+	switch(mov){
+		case 'x':
+			switch(tolower(piece)){
+				case 'p':
+					if(board[fx][fy] != '_' && (abs(fx - ix) == 1 && abs(fy - iy) == 1))
+						return 1;
+					else
+					return 0;
+				case 'b':
+					
+				case 'p':
+				case 'p':
+				case 'p':
+				case 'p':
+
+		case '-':
+	
+		
+
 }
