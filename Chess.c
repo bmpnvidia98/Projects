98946779f0546a60d7e61a8eeb1ad4c64834757b
bmpnvidia98 <moink1234@gmail.com>
2015-11-24 10:49:00 -0500

    Update Chess.c

--- Chess.c
+++ Chess.c
@@ -62,17 +62,45 @@ int isvalid(char piece, char ix1, char iy1, char mov, char fx1, char fy1){
 				case 'r':
 					if(board[fx][fy] != '_' && (!(fx-ix != 0) != !(fy - iy != 0))
 						return 1;
-                    return 0;
-                case 'n':
-                    if(board[fx][fy] != '_' && ((abs(fx - ix) == 3 && abs(fy - iy) == 1) || (abs(fy - iy) == 3 && abs(fx - ix) == 1)  ))
-                        return 1;
-                    return 0;
+                		    return 0;
+                		case 'n':
+                    			if(board[fx][fy] != '_' && ((abs(fx - ix) == 3 && abs(fy - iy) == 1) || (abs(fy - iy) == 3 && abs(fx - ix) == 1)  ))
+                				     return 1;
+                			  return 0;
 				case 'k':
-				    if(board[fx][fy] != '_' && )
-				case 'p':
-
+					if(board[fx][fy] != '_' && matecheck != 1 && (abs(ix-fx) == 1 || abs(iy-fy) == 1))
+						return 1;
+					return 0;
+				case 'q':
+					if(board[fx][fy] != '_' && (abs(ix-fx)%abs(iy-fy)==0 || (abs(ix-fx)%abs(iy-fy)== NULL)) )
+						return 1;
+					return 0;
+			}
 		case '-':
-
+				case 'p':
+					if(abs(fx - ix) == 1 && abs(fy - iy) == 1)
+						return 1;
+					return 0;
+				case 'b':
+					if((abs(fx-ix))/(abs(fy-iy)) == 1 && (fx != ix || fy != iy))
+						return 1;
+					return 0;
+				case 'r':
+					if((!(fx-ix != 0) != !(fy - iy != 0))
+						return 1;
+                		    return 0;
+                		case 'n':
+                    			if(((abs(fx - ix) == 3 && abs(fy - iy) == 1) || (abs(fy - iy) == 3 && abs(fx - ix) == 1)  ))
+                				     return 1;
+                			  return 0;
+				case 'k':
+					if(matecheck != 1 && (abs(ix-fx) == 1 || abs(iy-fy) == 1))
+						return 1;
+					return 0;
+				case 'q':
+					if((abs(ix-fx)%abs(iy-fy)==0 || (abs(ix-fx)%abs(iy-fy)== NULL)) )
+						return 1;
+					return 0;
 
 
 }
