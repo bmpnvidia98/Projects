19b4189c8cfe2be49188f8dbb4a16c40946f1790
Brian P <antoun@localhost.localdomain>
2015-11-28 10:20:52 -0500

    Fixed commentdel.c for all situations

--- commentdel.c
+++ commentdel.c
@@ -2,10 +2,12 @@
 
 #include <stdio.h>
 #include <string.h>
+#define DEFAULT 0
+#define QUOTES 1
 
 int main(int argc, char *argv[]){
 	char fname[20],fname1[20];
-	int c, prevc, prevc1;
+	int c, prevc;
 		if(argc < 2){
 		printf("You need to have a filename, in format ./commentdel (filename)\n");
 		return 0;
@@ -19,26 +21,40 @@ int main(int argc, char *argv[]){
 	fp = fopen(fname, "r");
 	fp1 = fopen(fname1, "w");
 	prevc = '0';
-	prevc1 = '0';
 	while((c = getc(fp)) != EOF){
 		if(c == '*' && prevc == '/'){
 			while((c = getc(fp)) != '/' || prevc != '*'){
 				prevc = c;
-				prevc1 = prevc;
 				}
 			}
 		else if(c == '/' && prevc != '/')
 			goto b;
-		else if(c == '/' && prevc == '/')
+		else if(c == '/' && prevc == '/'){
 			while(c != '\n'){
 				c = getc(fp);
-				prevc = c;
-				prevc1 = prevc;
+				prevc = c; 
 				}
+			fprintf(fp1, "%c", c);
+			}
+		else if(c == '"'){
+				fprintf(fp1, "%c", c);
+			while((c = getc(fp)) != '"'){
+				fprintf(fp1, "%c", c);
+				prevc = c;
+					}
+			fprintf(fp1, "%c", c);
+			}
+		else if(c == '\''){
+				fprintf(fp1, "%c", c);
+			while((c = getc(fp)) != '\''){
+				fprintf(fp1, "%c", c);
+				prevc = c;
+					}
+			fprintf(fp1, "%c", c);
+			}
 		else {fprintf(fp1, "%c", c);}
 		b:
 		prevc = c;
-		prevc1 = prevc;
 		}
 		
 	return 0;
